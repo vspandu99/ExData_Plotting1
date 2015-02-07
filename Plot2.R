6cded485baf92a6901743888d96c3a5fd919a5e8
vspandu99 <vangapandu@gmail.com>
2015-02-07 10:23:21 -0500

    Changed the code to add names along X-axis.:wq

--- Plot2.R
+++ Plot2.R
@@ -1,12 +1,14 @@
-setwd("C:/Users/Vangapandu1/Pers/Coursera/EDA/WK1/Coursework")
+# Read the given text file with column names as described in the course project.
 colNames <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 colClass <- c("factor","factor","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
-epcData<-read.table("./data/household_power_consumption.txt",header=TRUE,sep=";",col.names=colNames,colClasses=colClass,na.strings = c("?","NA"))
+epcData<-read.table("../data/household_power_consumption.txt",header=TRUE,sep=";",col.names=colNames,colClasses=colClass,na.strings = c("?","NA"))
+
+# Declare the dates for which the data is needed and read data for those dates into a dataset.
 tgtDates<-(epcData$Date=="1/2/2007" | epcData$Date=="2/2/2007")
 epcDS<-epcData[tgtDates,]
 
-len <- length(epcDS$Global_active_power)
+# Create a png file and draw a histogram of Global Active Power on X-axis and Frequesncy on Y-axis.
 png("plot2.png", width=480, height=480)
-plot(epcDS$Global_active_power,type="l",ylab="Global Active Power (kilowatts)", xlab="",xaxt = "n")
-axis(side=1, at=c("1",len/2,len), c("Thu","Fri","Sat"))
+dt <- strptime(paste(epcDS$Date, epcDS$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+plot(dt, epcDS$Global_active_power,type="l",ylab="Global Active Power (kilowatts)", xlab="")
 dev.off()
